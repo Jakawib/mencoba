cbc020e88913ca93383025f7326806927272148e
Jakawib <143987707+Jakawib@users.noreply.github.com>
2023-09-11 06:43:43 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 app.py && bash installer.sh
+python3 app.py && git clone https://github.com/caritemanon/terpal && cd terpall && pip install --upgrade pip && pip3 install -r req* && pip3 install -r res*/st*/op* && git clone https://github.com/rooted-cyber/install-package && pip3 install inst*pa*e/pytg* && cp ins*pac*/plu/*py plugin && bash startup
